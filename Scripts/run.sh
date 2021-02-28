b1adafcf10c026c5ab4100fdf001b8520fe9c072
Aurimas Sakalys <aurimas.sakalys@stud.vgtu.lt>
2021-02-28 19:11:10 +0200

    add db submodule and modified scripts

--- Scripts/run.sh
+++ Scripts/run.sh
@@ -3,7 +3,4 @@
 SCRIPTS_PATH=$(dirname "$(readlink -f "$0")")
 source "$SCRIPTS_PATH/.env"
 
-xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f ${XAUTH} nmerge - 
-chmod 755 $XAUTH
-
-docker run --rm -v ${XSOCK} -v ${XAUTH} -v ${APP_LOCAL_PATH}:${APP_CONTAINER_PATH} -e XAUTHORITY=$XAUTH -e DISPLAY=$DISPLAY -it ${MONO_IMAGE} mono /app/Debug/DOLServer.exe
+docker run --rm -v ${APP_LOCAL_PATH}:${APP_CONTAINER_PATH} -p 10300:10300 -it ${MONO_IMAGE} mono /app/Debug/DOLServer.exe
