b132c74c3d891782b0f20ae6c8a9353af1cf2ec3
Aurimas Sakalys <aurimas.sakalys@stud.vgtu.lt>
2021-02-28 19:43:31 +0200

    Database update script added

--- scripts/update-db.sh
+++ scripts/update-db.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 
+SCRIPTS_PATH=$(dirname "$(readlink -f "$0")")
+source "$SCRIPTS_PATH/.env"
+
+MIGRAION_REGEX="/^migration_[0-9]{14}\.sql/"
+MIGRATIONS=$(ls $MIGRATIONS_FOLDER | awk -e $MIGRAION_REGEX | wc -l)
+DB_CONCAT_DIR="$DATABASE_FOLDER/src/scripts/bin/linux/"
+
+echo "Migrations: ${MIGRATIONS}"
+
+cd "$DB_CONCAT_DIR"
+
+if [ $MIGRATIONS -eq 0 ]; then 
+    ./concat
+else
+    ./concat --update-only
+fi
+
+mv public-db.sql "$MIGRATIONS_FOLDER/migration_$(date -u +%Y%m%d%H%M%S).sql"
