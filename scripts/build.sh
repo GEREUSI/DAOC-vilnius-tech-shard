0a4463cbcca36eb84e987ef59fdacb3ad373d153
Aurimas Sakalys <aurimas.sakalys@stud.vgtu.lt>
2021-04-07 16:53:58 +0300

    Script updates.

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,4 +4,10 @@ SCRIPTS_PATH="$(dirname "$(readlink -f "$0")")"
 ENV_PATH="$SCRIPTS_PATH/../.env"
 source $ENV_PATH
 
-docker run --rm -v ${APP_LOCAL_PATH}:${APP_CONTAINER_PATH} -v ${BUILD_SCRIPTS_LOCAL_PATH}:${BUILD_SCRIPTS_CONTAINER_PATH} -it ${MONO_IMAGE} ${BUILD_SCRIPTS_CONTAINER_PATH}/build.sh
+if [ "$1" == "local" ]; then
+    docker run --rm -v ${APP_LOCAL_PATH}:${APP_CONTAINER_PATH} -v ${BUILD_SCRIPTS_LOCAL_PATH}:${BUILD_SCRIPTS_CONTAINER_PATH} -it ${MONO_IMAGE} ${BUILD_SCRIPTS_CONTAINER_PATH}/build.sh
+elif [ "$1" == "remote" ]; then
+    docker run --rm -v ${APP_LOCAL_PATH}:${APP_CONTAINER_PATH} -v ${BUILD_SCRIPTS_LOCAL_PATH}:${BUILD_SCRIPTS_CONTAINER_PATH} ${MONO_IMAGE} ${BUILD_SCRIPTS_CONTAINER_PATH}/build.sh
+else
+    echo "No such option"
+fi
